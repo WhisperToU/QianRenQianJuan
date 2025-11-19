fe5dbcba0992eb13d33869508a66784e8db38e4d
王敬晨 <2076337074@qq.com>
2025-11-19 15:29:29 +0800

    前端概念设计

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ app = Flask(__name__)
 CORS(app)
 
 # 注册功能模块（Blueprints）
-app.register_blueprint(questions_bp, url_prefix='/questions')
-app.register_blueprint(printing_bp, url_prefix='/printing')
-app.register_blueprint(correction_bp, url_prefix='/correction')
-app.register_blueprint(assign_bp, url_prefix='/assign')
-app.register_blueprint(students_bp, url_prefix='/students')
-app.register_blueprint(classes_bp, url_prefix='/classes')
+app.register_blueprint(questions_bp, url_prefix='/questions')#支持数据库的增删改查
+app.register_blueprint(printing_bp, url_prefix='/printing')#支持根据题目ID集合生成docx试卷与答案
+app.register_blueprint(correction_bp, url_prefix='/correction')#面向教师铲鲟学生历史答题记录
+app.register_blueprint(assign_bp, url_prefix='/assign')#支持给学生分配试题
+app.register_blueprint(students_bp, url_prefix='/students')#获取数据库中学生信息
+app.register_blueprint(classes_bp, url_prefix='/classes')#获取数据库中班级信息
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
